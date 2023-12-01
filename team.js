d44f2a2b22be021640d70f99eacfc3f87775c930
valentina Li <valentinalidev@gmail.com>
2023-12-01 14:58:44 +0100

    added obj valentina

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-const persons = [];
+const persons = [
+    {
+        name: "Valentina",
+        surname: "Li",
+        age: 22,
+        city: "Roma",
+        hobby: "Gym",
+        favoriteFood: "Sushi",
+        favoriteVideoGame: "League of Legends",
+        favoriteFilm: "",
+        favoriteBook: "Prima di te nessuno mai",
+        petName: "Yuki",
+    },
+];
